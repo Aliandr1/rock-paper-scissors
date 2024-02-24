627aa5465bf22bcbc7773bbd9131ecce4233b53d
Aliandr1 <152308963+Aliandr1@users.noreply.github.com>
2024-02-24 15:30:18 +0100

    finish implementation of the game

--- index.js
+++ index.js
@@ -1,46 +1,78 @@
-const userChoice = prompt('Choose rock paper or scissors');
+let userChoice;
 
-const choiceOne = 'rock';
-const choiceTwo = 'paper';
-const choiceThree = 'scissors';
+const choiceOne = "rock";
+const choiceTwo = "paper";
+const choiceThree = "scissors";
 
 function getComputerChoice(one, two, three) {
- let random = Math.random()
-   if(random <= 0.34) {
-    return 'rock';
-   } else if(random <= 0.67) {
-    return 'paper'
-   } else {
-    return 'scissors';
-   }
-   
+  let random = Math.random();
+  if (random <= 0.34) {
+    return "rock";
+  } else if (random <= 0.67) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
- 
 const computerChoice = getComputerChoice(choiceOne, choiceTwo, choiceThree);
- 
 
+let userScore = 0;
+let computerScore = 0;
 
-if(userChoice === 'rock' && computerChoice === 'rock') {
-    console.log("It's a draw. Play again");
-    prompt('Choose rock paper or scissors');
-} else if(userChoice === 'rock' && computerChoice === 'scissors') {
-     console.log('Rock beats scissors! You win');
-} else if(userChoice === 'rock' && computerChoice === 'paper') {
-    console.log('Paper beats rock! You lose');
-}    else if(userChoice === 'scissors' && computerChoice === 'rock') {
-        console.log('Rock beats scissors! You lose');
- } else if(userChoice === 'scissors' && computerChoice === 'paper') {
-    console.log('Scissors beat paper! You win');
- } else if(userChoice === 'scissors' && computerChoice === 'scissors') {
-    console.log("It's a draw. Play again");
- } else if(userChoice === 'paper' && computerChoice === 'rock') {
-    console.log('Paper beats rock! You win');
- } else if(userChoice === 'paper' && computerChoice === 'scissors') {
-    console.log('Scissors beat paper! You lose');
- } else if(userChoice === 'paper' && computerChoice === 'paper') {
-    console.log("It's a draw. Play again");
- } 
+function playRound(a, b) {
+  if (userChoice === "rock" && computerChoice === "rock") {
+    console.log(`It's a draw! Play again ${userScore} vs ${computerScore}`);
+    return [userScore, computerScore];
+  } else if (userChoice === "rock" && computerChoice === "scissors") {
+    console.log(
+      `Rock beats scissors! You win ${++userScore} vs ${computerScore}`
+    );
+    return [userScore, computerScore];
+  } else if (userChoice === "rock" && computerChoice === "paper") {
+    console.log(
+      `Paper beats rock! You lose ${userScore} vs ${++computerScore}`
+    );
+    return [userScore, computerScore];
+  } else if (userChoice === "scissors" && computerChoice === "rock") {
+    console.log(
+      `Rock beats scissors! You lose ${userScore} vs ${++computerScore}`
+    );
+    return [userScore, computerScore];
+  } else if (userChoice === "scissors" && computerChoice === "paper") {
+    console.log(
+      `Scissors beat paper! You win ${++userScore} vs ${computerScore}`
+    );
+    return [userScore, computerScore];
+  } else if (userChoice === "scissors" && computerChoice === "scissors") {
+    console.log(`It's a draw. Play again ${userScore} vs ${computerScore}`);
+    return [userScore, computerScore];
+  } else if (userChoice === "paper" && computerChoice === "rock") {
+    console.log(`Paper beats rock! You win ${++userScore} vs ${computerScore}`);
+    return [userScore, computerScore];
+  } else if (userChoice === "paper" && computerChoice === "scissors") {
+    console.log(
+      `Scissors beat paper! You lose ${userScore} vs ${++computerScore}`
+    );
+    return [userScore, computerScore];
+  } else if (userChoice === "paper" && computerChoice === "paper") {
+    console.log(`It's a draw. Play again ${userScore} vs ${computerScore}`);
+    return [userScore, computerScore];
+  }
+}
 
-console.log(userChoice, computerChoice);
+for (let i = 0; i < 3; i++) {
+  userChoice = prompt("Choose rock paper or scissors");
+  console.log(playRound(userChoice, computerChoice));
+}
 
+while (userScore === computerScore) {
+  userChoice = prompt("Choose rock paper or scissors");
+  console.log(playRound(userChoice, computerChoice));
+}
+
+if (userScore > computerScore) {
+  console.log("You won the game! CONGRATULATIONS!!!");
+} else {
+  console.log("You lost the game :( Try again maybe");
+}
